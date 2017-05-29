9cbe78393d9993fa93aa0fc2b88d65846bf12563
rtk <rohit_rtk@hotmail.com>
2017-05-28 20:32:36 -0400

    Attempting to add health to the player
    + Added a script to handle players health
    - Need to test networking with the UI to continue

---
+++ Project/Assets/Scripts/Player/Player_Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Player_Health : MonoBehaviour {
+
+    private const float _baseHealth = 100f;
+    [SerializeField] private float _playerHealth;
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Image _image;
+    [SerializeField] private Color _healthColour = Color.red;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _playerHealth = _baseHealth;
+        _slider.value = _playerHealth;
+        _image.color = _healthColour;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
